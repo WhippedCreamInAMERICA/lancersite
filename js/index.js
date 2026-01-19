b8a64a0db653eda99e58ec324c272a6514367a82
zarkologist <personal@zarkozy.com>
2026-01-19 18:44:51 -0500

    fix gpt code

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const lancerButton = document.getElementById("lancerButton");
+const lancerSound = document.getElementById("lancerSound");
+
+lancerButton.addEventListener("click", () => {lancerSound.play()});
